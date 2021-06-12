6a8e671dc6d9f103bded605743b2adde6ce966ad
l08084 <l08084.1989@gmail.com>
2021-06-12 14:45:37 +0900

    fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,28 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'test-eslint';
+  constructor() {
+    const oranges = ['ripe orange A ', 'green orange B', 'ripe orange C'];
+
+    let ripe_oranges = oranges.filter((fruit) =>
+      fruit.match(/(?<=ripe )orange/)
+    );
+    console.log(ripe_oranges);
+
+    const re = new RegExp('(?<=ripe )orange');
+
+    ripe_oranges = oranges.filter((fruit) => fruit.match(re));
+    console.log(ripe_oranges);
+
+    const str = '(?<=ripe )orange';
+
+    const re2 = new RegExp(str);
+
+    ripe_oranges = oranges.filter((fruit) => fruit.match(re2));
+    console.log(ripe_oranges);
+  }
 }
